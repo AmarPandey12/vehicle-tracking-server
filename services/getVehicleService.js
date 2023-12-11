d99bbfb10db601fc032e4b543cf07cd90511d306
Amar Nath Pandey <amar.61039035@ltimindtree.com>
2023-12-11 13:20:00 +0530

    updated vehicle service

--- services/getVehicleService.js
+++ services/getVehicleService.js
@@ -4,8 +4,6 @@ const request = require('request');
 
 const getVehicleServices = () =>{
     console.log(`welcome to get vehicle service`);
-    const token = '';
-
     var options = {
         'method': 'GET',
         'url': 'https://hst-api.wialon.com/wialon/ajax.html?svc=token/login&params={"token":"5560db8f4a2317dfb4690c5273a984098D637F85A0851AC11CC6D0450DC3A9CFB006FA40"}',
@@ -15,12 +13,13 @@ const getVehicleServices = () =>{
     request(options, function (error, response) {
     if (error) throw new Error(error);
         console.log(response.body.eid);
-        token = response.body.eid;
+        const token = response.body.eid;
+        return token;
         
     });
 
         // res.send('response from controller HI');
-    return token;
+    
     }
     
     
