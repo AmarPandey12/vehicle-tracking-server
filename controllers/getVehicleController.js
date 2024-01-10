71b6d96eb26afad34a0747d44a3979ba717c1e76
Amar Nath Pandey <amar.61039035@ltimindtree.com>
2024-01-10 10:40:00 +0530

    added location details

--- controllers/getVehicleController.js
+++ controllers/getVehicleController.js
@@ -3,46 +3,22 @@ const getVehicleService = require('../services/getVehicleService');
 
 const getVehicleDetails = async (req, res)=>{
     console.log('inside getVehicleDetails controller:: ');
-    // console.log(`receiving vehicle controller request body ${request.query.params}`);
-
     // Calling getVehicle service 
-    
     const vehicleData = JSON.parse(JSON.stringify(await getVehicleService.getVehicleServices()));
-    // res.send(vehicleData);
-
     let vehicleDetails = [];
     vehicleData.forEach((element, index) => {
         const vehicleSensorData = element.d.sens;
         let sensorData = [];
-        // console.log('<<<<<<<<<<<<<<<<');
         let sensorKey;
         for(const [i,val] of Object.entries(vehicleSensorData))
         {
-            // console.log('>>>>>>> ' + i, val);
             sensorKey = val.p;
-            console.log('((((((((((((((((((' + JSON.stringify(element.d.lmsg.p[sensorKey]));
             sensorData.push({'sensor': val.n , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
         }
-        
-        // (sensorData.find(({ key }) => key === sensorKey)) ? sensorData.push({'value': 'custom' + sensorKey}) : '';
-
-        // let finalData = sensorData.map((ele)=>{
-            
-        // });
-
-        // console.log('hello from sensor', result);
-        // console.log('>>>>>>>>>>>>>>>>');
-        
-        
-        
-        vehicleDetails.push({'vehicle': element.d.nm, 'vehicle Id': element.d.id, 'sensor data': sensorData, 'Time': new Date(element.d.pos.t)})
+        vehicleDetails.push({'vehicle': element.d.nm, 'vehicle_id': element.d.id, 'last_location_lat': element.d.lmsg.pos.x, 'last_location_long': element.d.lmsg.pos.y , 'sensor_data': sensorData, 'Time': new Date(element.d.pos.t)})
     });
-
     res.send(vehicleDetails);
-    
-    // return vehicleDetails;
 }
-
 module.exports = getVehicleDetails;
 
 
