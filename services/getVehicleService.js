7b0e034b73d934bb50a2e4d67fae833c92405ed8
Amar Nath Pandey <amar.61039035@ltimindtree.com>
2024-01-10 15:01:16 +0530

    added location details

--- services/getVehicleService.js
+++ services/getVehicleService.js
@@ -3,17 +3,17 @@ const axios = require('axios');
 
 const getVehicleServices = async (sidToken) =>{
     try {
-        const res = await axios({
-            method: 'get',
-            url:'https://hst-api.wialon.com/wialon/ajax.html',
-            params:{
-                svc: 'token/login',
-                params: '{"token": "'+sidToken+'"}'
-            }
-        });
+        // const res = await axios({
+        //     method: 'get',
+        //     url:'https://hst-api.wialon.com/wialon/ajax.html',
+        //     params:{
+        //         svc: 'token/login',
+        //         params: '{"token": "'+sidToken+'"}'
+        //     }
+        // });
 
-        const token = res.data.eid;
-        console.log('>>>>>>> ' + token);
+        // const token = res.data.eid;
+        // console.log('>>>>>>> ' + token);
         // Making vehicle detail call
         const vehicleDetails = await axios({
             method: 'get',
@@ -21,7 +21,7 @@ const getVehicleServices = async (sidToken) =>{
             params:{
                 svc: 'core/update_data_flags',
                 params: '{"spec":[{"type":"type","data":"avl_unit","flags":5121,"mode":0}]}',
-                sid: token
+                sid: sidToken
             }
         });
         const vehicleData = vehicleDetails.data;
