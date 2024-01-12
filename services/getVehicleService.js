9f0d976c4cdf927f1a26334bcbaa85cba87972f4
Amar Nath Pandey <amar.61039035@ltimindtree.com>
2024-01-12 11:59:55 +0530

    fetch sensors

--- services/getVehicleService.js
+++ services/getVehicleService.js
@@ -14,6 +14,23 @@ const getVehicleServices = async (sidToken) =>{
 
         const token = res.data.eid;
         console.log('>>>>>>> ' + token);
+
+        let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'https://hst-api.wialon.com/wialon/ajax.html?svc=core/update_data_flags&params={"spec":[{"type":"type","data":"avl_unit","flags":1025,"mode":0}]}&sid='+token+'"',
+        headers: { }
+        };
+        
+        axios.request(config)
+        .then((response) => {
+        const vehicleData = response.data;
+        console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+        console.log(error);
+        });
+
         // Making vehicle detail call
         // const vehicleDetails = await axios({
         //     method: 'get',
@@ -25,7 +42,7 @@ const getVehicleServices = async (sidToken) =>{
         //     }
         // });
         // const vehicleData = vehicleDetails.data;
-        console.log('????????? ' + token);
+        console.log('????????? ' + vehicleData);
         return token;
 
       } catch (error) {
