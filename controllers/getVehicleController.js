57bb13a2afca96cf631ed558f6ea234a0ac191ed
Amar Nath Pandey <amar.61039035@ltimindtree.com>
2024-01-31 15:30:49 +0530

    api endpont update

--- controllers/getVehicleController.js
+++ controllers/getVehicleController.js
@@ -10,18 +10,18 @@ const getVehicleDetails = async (req, res)=>{
     let vehicleDetails = [];
     // res.send(vehicleData);
     
-    vehicleData.forEach((element, index) => {
-        const vehicleSensorData = element.d.sens;
-        let sensorData = [];
-        let sensorKey;
-        for(const [i,val] of Object.entries(vehicleSensorData))
-        {
-            sensorKey = val.p;
-            sensorData.push({'sensor': val.n , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
-        }
-        vehicleDetails.push({'vehicle': element.d.nm, 'vehicle_id': element.d.id, 'last_location_lat': element.d.lmsg.pos.x, 'last_location_long': element.d.lmsg.pos.y , 'sensor_data': sensorData, 'Time': new Date(element.d.pos.t * 1000), 'org_timestamp': element.d.pos.t})
-    });
-    res.send(vehicleDetails);
+    // vehicleData.forEach((element, index) => {
+    //     const vehicleSensorData = element.d.sens;
+    //     let sensorData = [];
+    //     let sensorKey;
+    //     for(const [i,val] of Object.entries(vehicleSensorData))
+    //     {
+    //         sensorKey = val.p;
+    //         sensorData.push({'sensor': val.n , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
+    //     }
+    //     vehicleDetails.push({'vehicle': element.d.nm, 'vehicle_id': element.d.id, 'last_location_lat': element.d.lmsg.pos.x, 'last_location_long': element.d.lmsg.pos.y , 'sensor_data': sensorData, 'Time': new Date(element.d.pos.t * 1000), 'org_timestamp': element.d.pos.t})
+    // });
+    res.send(vehicleData);
 }
 module.exports = getVehicleDetails;
 
