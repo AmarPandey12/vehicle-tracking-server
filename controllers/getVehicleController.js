cfee3638f82b95167a84cb8be980358a43c3f1e9
Amar Nath Pandey <amar.61039035@ltimindtree.com>
2024-02-12 16:37:59 +0530

    updating sensors data

--- controllers/getVehicleController.js
+++ controllers/getVehicleController.js
@@ -28,30 +28,16 @@ const getVehicleDetails = async (req, res)=>{
                 }else if(sensorName == 'Ignition'){
                     console.log('SENSOR KEY 3' + sensorName);
                     sensorData.push({'engine_sensor': 'Engine' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
-                }else{
-                    sensorData.push({'engine_sensor': 'Engine' , 'key': 'N/A', 'value': 'N/A'});
-                }
-
-                // Get fuel volume total sensor volume
-                if(sensorName == 'Fuel Volume Total'){
+                }else if(sensorName == 'Fuel Volume Total'){
                     console.log('SENSOR KEY 1 ' + sensorName);                    
                     sensorData.push({'fuel_sensor': 'Fuel' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
-                }else{
-                    sensorData.push({'fuel_sensor': 'Fuel' , 'key': 'N/A', 'value': 'N/A'});
-                }
-
-                // Get power sully status
-                if(sensorName == 'Power Supply Status'){
+                }else if(sensorName == 'Power Supply Status'){
                     console.log('SENSOR KEY 2' + sensorName);
-                    sensorData.push({'power sensor': 'Power' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
+                    sensorData.push({'power_sensor': 'Power' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
                 }else if(sensorName == 'Supply Power'){
                     console.log('SENSOR KEY 3' + sensorName);
                     sensorData.push({'power_sensor': 'Power' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
-                }else{
-                    sensorData.push({'power_sensor': 'Power' , 'key': 'N/A', 'value': 'N/A'});
                 }
-                
-                
             }
             vehicleDetails.push({'vehicle': element.d.nm, 'vehicle_id': element.d.id, 'last_location_lat': element.d.lmsg.pos.x, 'last_location_long': element.d.lmsg.pos.y , 'sensor_data': sensorData, 'Time': new Date(element.d.pos.t * 1000), 'org_timestamp': element.d.pos.t})
         });
