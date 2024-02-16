d2405d520a17b4f1be2bf1a8b65c6d35379137c4
Amar Nath Pandey <amar.61039035@ltimindtree.com>
2024-02-16 14:16:23 +0530

    updating sensors data

--- controllers/getVehicleController.js
+++ controllers/getVehicleController.js
@@ -13,27 +13,27 @@ const getVehicleDetails = async (req, res)=>{
         res.send(vehicleData);
     
         vehicleData.forEach((element, index) => {
-            const vehicleSensorData = element.d.sens;
-            let sensorData = [];
-            let sensorKey;
-            for(const [i,val] of Object.entries(vehicleSensorData))
-            {
-                sensorKey = val.p;
-                // Get fuel volume total sensor volume
-                if(sensorKey == 'Fuel Volume Total'){
-                    
-                    sensorData.push({'sensor': 'Fuel' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
-                }
+            console.log('Check point 1');
+            // const vehicleSensorData = element.d.sens;
+            // let sensorData = [];
+            // let sensorKey;
+            // for(const [i,val] of Object.entries(vehicleSensorData))
+            // {
+            //     sensorKey = val.p;
+            //     // Get fuel volume total sensor volume
+            //     if(sensorKey == 'Fuel Volume Total'){
+            //         sensorData.push({'sensor': 'Fuel' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
+            //     }
 
-                // Get engine hour or Ignition
-                if(sensorKey == 'Engine Hour'){
-                    sensorData.push({'sensor': 'Engine' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
-                }else if(sensorKey == 'Ignition'){
-                    sensorData.push({'sensor': 'Engine' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
-                }
+            //     // Get engine hour or Ignition
+            //     if(sensorKey == 'Engine Hour'){
+            //         sensorData.push({'sensor': 'Engine' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
+            //     }else if(sensorKey == 'Ignition'){
+            //         sensorData.push({'sensor': 'Engine' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
+            //     }
                 
                 
-            }
+            // }
             vehicleDetails.push({'vehicle': element.d.nm, 'vehicle_id': element.d.id, 'last_location_lat': element.d.lmsg.pos.x, 'last_location_long': element.d.lmsg.pos.y , 'sensor_data': sensorData, 'Time': new Date(element.d.pos.t * 1000), 'org_timestamp': element.d.pos.t})
         });
         res.send(vehicleDetails);
