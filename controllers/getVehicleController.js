869ac8b7b92e7d4f4314fbb17f50efffdffc2fe3
Amar Nath Pandey <amar.61039035@ltimindtree.com>
2024-02-19 11:05:25 +0530

    updating sensors data

--- controllers/getVehicleController.js
+++ controllers/getVehicleController.js
@@ -10,50 +10,33 @@ const getVehicleDetails = async (req, res)=>{
         const vehicleData = JSON.parse(JSON.stringify(await getVehicleService.getVehicleServices(sid)));
         let vehicleDetails = [];
         // console.log(vehicleData);
-        // res.send(vehicleData);
+        res.send(vehicleData);
     
         vehicleData.forEach((element, index) => {
             console.log('Check point 1');
-            // const vehicleSensorData = element.d.sens;
-            // let sensorData = [];
-            // let sensorKey;
+            const vehicleSensorData = element.d.sens;
+            let sensorData = [];
+            let sensorKey;
             // for(const [i,val] of Object.entries(vehicleSensorData))
             // {
-            //     sensorKey = val.p;
-            //     // Get fuel volume total sensor volume
-            //     if(sensorKey == 'Fuel Volume Total'){
-            //         sensorData.push({'sensor': 'Fuel' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
-            //     }
-
             //     // Get engine hour or Ignition
             //     if(sensorKey == 'Engine Hour'){
             //         sensorData.push({'sensor': 'Engine' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
             //     }else if(sensorKey == 'Ignition'){
             //         sensorData.push({'sensor': 'Engine' , 'key': sensorKey, 'value': element.d.lmsg.p[sensorKey]});
             //     }
-                
-                
             // }
-            // vehicleDetails.push({'vehicle': element.d.nm, 'vehicle_id': element.d.id, 'last_location_lat': element.d.lmsg.pos.x, 'last_location_long': element.d.lmsg.pos.y , 'sensor_data': sensorData, 'Time': new Date(element.d.pos.t * 1000), 'org_timestamp': element.d.pos.t})
+            
             vehicleDetails.push({
                 'vehicle': element.d.nm, 
                 'vehicle_id': element.d.id, 
                 'last_location_lat': (element.d.lmsg.pos) ? element.d.lmsg.pos.x : 'N/A',
                 'last_location_long': (element.d.lmsg.pos)  ? element.d.lmsg.pos.x : 'N/A',
                 'Time': new Date(element.d.pos.t * 1000), 
-                'org_timestamp': element.d.pos.t
+                'org_timestamp': element.d.pos.t,
+                'sensor_data': sensorData
             })
 
-            // if(element.d.lmsg.pos){
-            //     vehicleDetails.push({
-            //         'last_location_lat': element.d.lmsg.pos.x
-            //         'last_location_long': element.d.lmsg.pos.y
-            //     });
-                
-            // }
-
-            
-            
         });
 
         console.log(vehicleDetails);
