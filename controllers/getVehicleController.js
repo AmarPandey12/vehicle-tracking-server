ee878bcfa58199e8bfbcfc5d95a95d532dd4ba6d
amar <amar.pandey@mindtree.com>
2024-03-18 16:43:19 +0530

    logic for fuel volume total

--- controllers/getVehicleController.js
+++ controllers/getVehicleController.js
@@ -44,7 +44,8 @@ const getVehicleDetails = async (req, res)=>{
                                     console.log('checkpoint 2', sensor_value_received, array[index].x, array[index + 1].x);
                                     RANGE_FOUND = inRange(sensor_value_received, array[index].x, array[index + 1].x);
                                     console.log('Range ', RANGE_FOUND);
-                                    FINAL_SENSOR_STATUS = (RANGE_FOUND) ? (array[i]?.b) ? 1 : 0 : 0
+                                    // FINAL_SENSOR_STATUS = (RANGE_FOUND) ? (array[i]?.b) ? 1 : 0 : 0
+                                    FINAL_SENSOR_STATUS = (RANGE_FOUND) ? 1 : 0;
                                     console.log(FINAL_SENSOR_STATUS);
 
                                     
